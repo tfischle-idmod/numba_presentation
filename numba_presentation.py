c60c1eaa36546012cdf65f479d588dd8e8310232
INTERNAL\tfischle <tfischle@idmod.org>
2021-06-23 06:45:27 -0700

    remove file

--- numba_presentation.py
+++
@@ -1,6 +0,0 @@
-def sum_all(A):
-    """Naive sum of elements of an array... assumes one dimensional array of floats"""
-    acc = 0.0
-    for i in xrange(A.shape[0]):
-        acc += A[i]
-    return acc
